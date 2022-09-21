3578b17f342c5e17333ebdeed16c636d48a35715
NakimuliSusan <nakimulimarysusan@gmail.com>
2022-09-21 16:06:21 +0300

    fetching data using reactjs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Kiosk from "./Kiosk";
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+     <Kiosk/>
+    </div>
+  );
+}
+
+export default App;
